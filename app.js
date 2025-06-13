29be57e2452c0c1942c06acfe3359dc15ed255eb
Matheus <reboucas444@gmail.com>
2025-06-13 11:42:31 -0300

    adicionando tentativas no codigo js

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ alert('Boas vindo ao jogo do numero aleatorio');
 
 let numeroSecreto = 5;
 let numeroRecebidoPeloUsuario;
+let tentativas = 0;
 
 while (numeroRecebidoPeloUsuario != numeroSecreto) {
      numeroRecebidoPeloUsuario = prompt('Escolha um numero entre 1 e 10');
 
      if (numeroRecebidoPeloUsuario == numeroSecreto) {
-          alert(`Isso ai, você descobriu o numero secreto ${numeroSecreto}`);
+          alert(`Isso ai, você descobriu o numero secreto ${numeroSecreto}, com ${tentativas} tentativas`);
      } else {
           if (numeroRecebidoPeloUsuario > numeroSecreto) {
                alert(`O número secreto é menor que ${numeroRecebidoPeloUsuario}`);
@@ -15,4 +16,5 @@ while (numeroRecebidoPeloUsuario != numeroSecreto) {
                alert(`O número secreto é maior que ${numeroRecebidoPeloUsuario}`);
           }
      }
+     tentativas++;
 }
