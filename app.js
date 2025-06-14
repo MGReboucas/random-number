22425012d970e04191ead41ba2711f2e57b39f36
Matheus <reboucas444@gmail.com>
2025-06-14 13:43:38 -0300

    trocando operador if e else por operadores trnarios na palavra tentativa e configuração do numero aleatorio

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 alert('Boas vindo ao jogo do numero aleatorio');
 
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100) + 1;
 let numeroRecebidoPeloUsuario;
-let tentativas = 0;
+let tentativas = 1;
 
 while (numeroRecebidoPeloUsuario != numeroSecreto) {
-     numeroRecebidoPeloUsuario = prompt('Escolha um numero entre 1 e 10');
+     numeroRecebidoPeloUsuario = prompt('Escolha um numero entre 1 e 100');
 
      if (numeroRecebidoPeloUsuario == numeroSecreto) {
-          alert(`Isso ai, você descobriu o numero secreto ${numeroSecreto}, com ${tentativas} tentativas`);
+          break;
      } else {
           if (numeroRecebidoPeloUsuario > numeroSecreto) {
                alert(`O número secreto é menor que ${numeroRecebidoPeloUsuario}`);
@@ -18,3 +18,6 @@ while (numeroRecebidoPeloUsuario != numeroSecreto) {
      }
      tentativas++;
 }
+//operador ternario - depois da interrogação será verdadeiro e caso não seja depois dos dois pontos
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você venceu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
